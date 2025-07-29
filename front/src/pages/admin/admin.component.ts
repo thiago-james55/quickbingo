ea88d381a3eb2e67b97ae1c7c2135fac50289d0d
Thiago Gonçalves <tsyhwh55@gmail.com>
2025-07-28 22:36:58 -0300

    Some minor design e other changes!

--- front/src/pages/admin/admin.component.ts
+++ front/src/pages/admin/admin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../layouts/header/header.component";
 import { FooterComponent } from '../../layouts/footer/footer.component';
+import { MainComponent } from "../../layouts/main/main.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
